912853dc49cd145fe24b1e451d9d92781f7bab9c
delilacruz <decruz@davidson.edu>
2025-12-18 22:59:58 -0500

    yay it's done

--- assets/js/main.js
+++ assets/js/main.js
@@ -89,11 +89,11 @@ ScrollTrigger.create({
   }
 });
 
-/* ScrollTrigger.create({
+ScrollTrigger.create({
   trigger: ".user-stories",
   pin: true, // pin the trigger element while active
   start: "top top", // when the top of the trigger hits the top of the viewport
-  end: "+=925", // end after scrolling 500px beyond the start
+  end: "+=1000", // end after scrolling 500px beyond the start
   // endTrigger: ".expected",
   // end: 'bottom 50%+=100px',
   // once: true,
@@ -108,8 +108,250 @@ ScrollTrigger.create({
       self.getVelocity()
     );
   }
-}); */
+});
+
+ScrollTrigger.create({
+  trigger: ".expected",
+  pin: true, // pin the trigger element while active
+  start: "top top", // when the top of the trigger hits the top of the viewport
+  end: "+=250", // end after scrolling 500px beyond the start
+  // endTrigger: ".expected",
+  // end: 'bottom 50%+=100px',
+  // once: true,
+  onToggle: (self) => console.log("toggled, isActive:", self.isActive),
+  onUpdate: (self) => {
+    console.log(
+      "progress:",
+      self.progress.toFixed(3),
+      "direction:",
+      self.direction,
+      "velocity",
+      self.getVelocity()
+    );
+  }
+});
 
 
 
+ScrollTrigger.create({
+  trigger: ".u-strug",
+  pin: true, // pin the trigger element while active
+  start: "top top", // when the top of the trigger hits the top of the viewport
+  end: "+=500", // end after scrolling 500px beyond the start
+  // endTrigger: ".expected",
+  // end: 'bottom 50%+=100px',
+  // once: true,
+  onToggle: (self) => console.log("toggled, isActive:", self.isActive),
+  onUpdate: (self) => {
+    console.log(
+      "progress:",
+      self.progress.toFixed(3),
+      "direction:",
+      self.direction,
+      "velocity",
+      self.getVelocity()
+    );
+  }
+});
+
+ScrollTrigger.create({
+  trigger: ".r-strug",
+  pin: true, // pin the trigger element while active
+  start: "top top", // when the top of the trigger hits the top of the viewport
+  end: "+=500", // end after scrolling 500px beyond the start
+  // endTrigger: ".expected",
+  // end: 'bottom 50%+=100px',
+  // once: true,
+  onToggle: (self) => console.log("toggled, isActive:", self.isActive),
+  onUpdate: (self) => {
+    console.log(
+      "progress:",
+      self.progress.toFixed(3),
+      "direction:",
+      self.direction,
+      "velocity",
+      self.getVelocity()
+    );
+  }
+});
+
+ScrollTrigger.create({
+  trigger: ".so-what",
+  pin: true,
+  start: "top top", 
+  end: "+=500", 
+  onToggle: (self) => console.log("toggled, isActive:", self.isActive),
+  onUpdate: (self) => {
+    console.log(
+      "progress:",
+      self.progress.toFixed(3),
+      "direction:",
+      self.direction,
+      "velocity",
+      self.getVelocity()
+    );
+  }
+});
+
+ScrollTrigger.create({
+  trigger: ".well-yes",
+  pin: true,
+  start: "top top", 
+  end: "+=500", 
+  onToggle: (self) => console.log("toggled, isActive:", self.isActive),
+  onUpdate: (self) => {
+    console.log(
+      "progress:",
+      self.progress.toFixed(3),
+      "direction:",
+      self.direction,
+      "velocity",
+      self.getVelocity()
+    );
+  }
+});
+
+ScrollTrigger.create({
+  trigger: ".however",
+  pin: true,
+  start: "top top", 
+  end: "+=500", 
+  onToggle: (self) => console.log("toggled, isActive:", self.isActive),
+  onUpdate: (self) => {
+    console.log(
+      "progress:",
+      self.progress.toFixed(3),
+      "direction:",
+      self.direction,
+      "velocity",
+      self.getVelocity()
+    );
+  }
+});
+
+ScrollTrigger.create({
+  trigger: ".gwe",
+  pin: true,
+  start: "top top", 
+  end: "+=600", 
+  onToggle: (self) => console.log("toggled, isActive:", self.isActive),
+  onUpdate: (self) => {
+    console.log(
+      "progress:",
+      self.progress.toFixed(3),
+      "direction:",
+      self.direction,
+      "velocity",
+      self.getVelocity()
+    );
+  }
+});
+
+ScrollTrigger.create({
+  trigger: ".gawp",
+  pin: true,
+  start: "top top", 
+  end: "+=600", 
+  onToggle: (self) => console.log("toggled, isActive:", self.isActive),
+  onUpdate: (self) => {
+    console.log(
+      "progress:",
+      self.progress.toFixed(3),
+      "direction:",
+      self.direction,
+      "velocity",
+      self.getVelocity()
+    );
+  }
+});
+
+ScrollTrigger.create({
+  trigger: ".children",
+  pin: true,
+  start: "top top", 
+  end: "+=600", 
+  onToggle: (self) => console.log("toggled, isActive:", self.isActive),
+  onUpdate: (self) => {
+    console.log(
+      "progress:",
+      self.progress.toFixed(3),
+      "direction:",
+      self.direction,
+      "velocity",
+      self.getVelocity()
+    );
+  }
+});
+
+ScrollTrigger.create({
+  trigger: ".huh",
+  pin: true,
+  start: "top top", 
+  end: "+=500", 
+  onToggle: (self) => console.log("toggled, isActive:", self.isActive),
+  onUpdate: (self) => {
+    console.log(
+      "progress:",
+      self.progress.toFixed(3),
+      "direction:",
+      self.direction,
+      "velocity",
+      self.getVelocity()
+    );
+  }
+});
+
+ScrollTrigger.create({
+  trigger: ".reprise",
+  pin: true,
+  start: "top top", 
+  end: "+=1000", 
+  onToggle: (self) => console.log("toggled, isActive:", self.isActive),
+  onUpdate: (self) => {
+    console.log(
+      "progress:",
+      self.progress.toFixed(3),
+      "direction:",
+      self.direction,
+      "velocity",
+      self.getVelocity()
+    );
+  }
+});
+
+ScrollTrigger.create({
+  trigger: ".different",
+  pin: true,
+  start: "top top", 
+  end: "+=500", 
+  onToggle: (self) => console.log("toggled, isActive:", self.isActive),
+  onUpdate: (self) => {
+    console.log(
+      "progress:",
+      self.progress.toFixed(3),
+      "direction:",
+      self.direction,
+      "velocity",
+      self.getVelocity()
+    );
+  }
+});
+
+ScrollTrigger.create({
+  trigger: ".chance",
+  pin: true,
+  start: "top top", 
+  end: "+=500", 
+  onToggle: (self) => console.log("toggled, isActive:", self.isActive),
+  onUpdate: (self) => {
+    console.log(
+      "progress:",
+      self.progress.toFixed(3),
+      "direction:",
+      self.direction,
+      "velocity",
+      self.getVelocity()
+    );
+  }
+});
 
