abff92574bb810eae23236547246121210dd3f55
delilacruz <decruz@davidson.edu>
2025-12-19 12:15:07 -0500

    fix responsiveness - scrollTrigger

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,12 +7,9 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
 ScrollTrigger.create({
   trigger: ".metaphor",
-  pin: true, // pin the trigger element while active
-  start: "top top", // when the top of the trigger hits the top of the viewport
-  end: "+=600", // end after scrolling 500px beyond the start
-  // endTrigger: 'box3D',
-  // end: 'bottom 50%+=100px',
-  // once: true,
+  pin: true, 
+  start: "top top", 
+  end: "+=600", 
   onToggle: (self) => console.log("toggled, isActive:", self.isActive),
   onUpdate: (self) => {
     console.log(
@@ -28,12 +25,9 @@ ScrollTrigger.create({
 
 ScrollTrigger.create({
   trigger: ".think",
-  pin: true, // pin the trigger element while active
-  start: "top top", // when the top of the trigger hits the top of the viewport
-  end: "+=1250", // end after scrolling 500px beyond the start
-  // endTrigger: 'box3D',
-  // end: 'bottom 50%+=100px',
-  // once: true,
+  pin: true, 
+  start: "top top", 
+  end: "+=1250", 
   onToggle: (self) => console.log("toggled, isActive:", self.isActive),
   onUpdate: (self) => {
     console.log(
@@ -49,12 +43,9 @@ ScrollTrigger.create({
 
 ScrollTrigger.create({
   trigger: ".invitation",
-  pin: true, // pin the trigger element while active
-  start: "top top", // when the top of the trigger hits the top of the viewport
-  end: "+=500", // end after scrolling 500px beyond the start
-  // endTrigger: 'box3D',
-  // end: 'bottom 50%+=100px',
-  // once: true,
+  pin: true, 
+  start: "top top", 
+  end: "+=500", 
   onToggle: (self) => console.log("toggled, isActive:", self.isActive),
   onUpdate: (self) => {
     console.log(
@@ -70,12 +61,9 @@ ScrollTrigger.create({
 
 ScrollTrigger.create({
   trigger: ".meet",
-  pin: true, // pin the trigger element while active
-  start: "top top", // when the top of the trigger hits the top of the viewport
-  end: "+=600", // end after scrolling 500px beyond the start
-  // endTrigger: 'box3D',
-  // end: 'bottom 50%+=100px',
-  // once: true,
+  pin: true, 
+  start: "top top", 
+  end: "+=600", 
   onToggle: (self) => console.log("toggled, isActive:", self.isActive),
   onUpdate: (self) => {
     console.log(
@@ -89,14 +77,15 @@ ScrollTrigger.create({
   }
 });
 
-ScrollTrigger.create({
+
+
+let mm = gsap.matchMedia();
+mm.add("(min-width: 600px)", () => {
+  ScrollTrigger.create({
   trigger: ".user-stories",
-  pin: true, // pin the trigger element while active
-  start: "top top", // when the top of the trigger hits the top of the viewport
-  end: "+=1000", // end after scrolling 500px beyond the start
-  // endTrigger: ".expected",
-  // end: 'bottom 50%+=100px',
-  // once: true,
+  pin: true, 
+  start: "top top", 
+  end: "+=1000", 
   onToggle: (self) => console.log("toggled, isActive:", self.isActive),
   onUpdate: (self) => {
     console.log(
@@ -109,15 +98,14 @@ ScrollTrigger.create({
     );
   }
 });
+})
+
 
 ScrollTrigger.create({
   trigger: ".expected",
-  pin: true, // pin the trigger element while active
-  start: "top top", // when the top of the trigger hits the top of the viewport
-  end: "+=250", // end after scrolling 500px beyond the start
-  // endTrigger: ".expected",
-  // end: 'bottom 50%+=100px',
-  // once: true,
+  pin: true, 
+  start: "top top", 
+  end: "+=250", 
   onToggle: (self) => console.log("toggled, isActive:", self.isActive),
   onUpdate: (self) => {
     console.log(
@@ -135,12 +123,9 @@ ScrollTrigger.create({
 
 ScrollTrigger.create({
   trigger: ".u-strug",
-  pin: true, // pin the trigger element while active
-  start: "top top", // when the top of the trigger hits the top of the viewport
-  end: "+=500", // end after scrolling 500px beyond the start
-  // endTrigger: ".expected",
-  // end: 'bottom 50%+=100px',
-  // once: true,
+  pin: true, 
+  start: "top top", 
+  end: "+=500", 
   onToggle: (self) => console.log("toggled, isActive:", self.isActive),
   onUpdate: (self) => {
     console.log(
@@ -156,12 +141,9 @@ ScrollTrigger.create({
 
 ScrollTrigger.create({
   trigger: ".r-strug",
-  pin: true, // pin the trigger element while active
-  start: "top top", // when the top of the trigger hits the top of the viewport
-  end: "+=500", // end after scrolling 500px beyond the start
-  // endTrigger: ".expected",
-  // end: 'bottom 50%+=100px',
-  // once: true,
+  pin: true, 
+  start: "top top", 
+  end: "+=500", 
   onToggle: (self) => console.log("toggled, isActive:", self.isActive),
   onUpdate: (self) => {
     console.log(
